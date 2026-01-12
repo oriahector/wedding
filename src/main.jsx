f900f58cf7b4f877c686718ef9bb542dc513314c
Héctor <hector@Hectors-MacBook-Air.local>
2026-01-12 21:55:19 +0100

    clarify added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Clarity from '@microsoft/clarity'
 import './index.css'
 import App from './App.jsx'
+
+// Initialize Microsoft Clarity
+Clarity.init('v0gtwz9ri8')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
